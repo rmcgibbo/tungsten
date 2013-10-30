4e314705fdad2484a84ee960a917519de2892879
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-29 17:51:01 -0700

    Add docstrings for MPI reductions

--- include/MPIReductions.hpp
+++ include/MPIReductions.hpp
@@ -33,10 +33,40 @@ typedef struct {
     float value;
 } triplet;
 
+
+/**
+ * Find the argmax of a distributed set of vectors.
+ *
+ * @param input       Each MPI rank calls this function with one vector
+ * @return triplet    The return value gives the global index (rank, index)
+ *                    of the max value, and that value itself.
+ */
 triplet MPI_vectorAllMaxloc(const std::vector<float>& input);
 
+/**
+ * Add a distributed collection of sparse matrices. All of the
+ * matrices _must_ have the same dimensions.
+ *
+ * Both MPI_csAdd and MPI_csAdd_efficient do the same thing, but have
+ * different implementations in terms of the order of the communication
+ * between the MPI processes.
+ *
+ * @param m          Each MPI rank calls this function with one sparse matrix
+ * @return sum       The result is the sum of the matrices
+ */
 cs* MPI_csAdd_efficient(cs* m);
 
+/**
+ * Add a distributed collection of sparse matrices. All of the
+ * matrices _must_ have the same dimensions.
+ *
+ * Both MPI_csAdd and MPI_csAdd_efficient do the same thing, but have
+ * different implementations in terms of the order of the communication
+ * between the MPI processes.
+ *
+ * @param m          Each MPI rank calls this function with one sparse matrix
+ * @return sum       The result is the sum of the matrices
+ */
 cs* MPI_csAdd(cs* m);
 
 } // namespace Tungsten
